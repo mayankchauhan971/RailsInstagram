fac88b2d3fdccb0e30cdf8fa95dbea13c12c9700
mayank <mayankchauhan971@gmail.com>
2020-05-14 03:22:38 +0530

    Added CRUD functionality

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  #to avoid adding default image
+  validates :image, presence: true
+
+  has_attached_file :image,  styles: { medium: "300x300" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+end
