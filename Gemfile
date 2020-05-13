8a9750f2acbced58e502252fe00e4150e9076cb5
mayank <mayankchauhan971@gmail.com>
2020-05-14 04:35:19 +0530

    Added css styles without bootstap

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'paperclip', '~> 6.0.0'
 gem 'rails', '~> 6.0.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
+# gem 'bootstrap', '~> 4.4.1'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   #to avoid adding default image
   validates :image, presence: true
 
-  has_attached_file :image,  styles: { medium: "300x300" }
+  has_attached_file :image,  styles: { medium: "450x450" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
